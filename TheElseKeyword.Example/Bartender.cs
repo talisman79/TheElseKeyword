4ee95fd4650b7900f7ae42bbb6c72b152788195f
KutukovAV <ka_2005@mail.ru>
2021-02-26 12:12:50 +0300

    Рефакторинг кода в соответствии с принципом open close.

--- TheElseKeyword.Example/Bartender.cs
+++ TheElseKeyword.Example/Bartender.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace TheElseKeyword.Example
 {
@@ -6,68 +7,28 @@ namespace TheElseKeyword.Example
     {
         private readonly Func<string> _inputProvider;
         private readonly Action<string> _outputProvider;
+        private readonly RecipeBook _recipeBook;
 
-        public Bartender(Func<string> inputProvider, Action<string> outputProvider)
+        public Bartender(Func<string> inputProvider, Action<string> outputProvider, RecipeBook recipeBook)
         {
             _inputProvider = inputProvider;
             _outputProvider = outputProvider;
+            _recipeBook = recipeBook;
         }
         
         public void AskForDrink()
         {
-            _outputProvider("What drink to you want? (beer, juice)");
+            _outputProvider($"What drink to you want? ({string.Join(", ", _recipeBook.GetAvailableDrinkNames())})");
 
             var drink = _inputProvider() ?? string.Empty;
-            switch (drink)
+
+            if (!_recipeBook.GetAvailableDrinkNames().Contains(drink))
             {
-                case "beer":
-                    ServeBeer();
-                    break;
-                case "juice":
-                    ServeJuice();
-                    break;
-                default:
-                    UnavailableDrink(drink);
-                    break;
-            }
-        }
-
-        private void UnavailableDrink(string drink)
-        {
-            _outputProvider($"Sorry mate but we don't do {drink}");
-        }
-
-        private void ServeJuice()
-        {
-            _outputProvider("Here you go! Fresh and nice juice.");
-        }
-
-        private void ServeBeer()
-        {
-            _outputProvider("Not so fast cowboy. How old are you?");
-            if (!int.TryParse(_inputProvider(), out var age))
-            {
-                HandleInvalidAge();
+                _outputProvider("Sorry I don't know how to make this one.");
                 return;
             }
             
-            HandleBeerAgeCheck(age);
-        }
-
-        private void HandleBeerAgeCheck(int age)
-        {
-            if (age >= 18)
-            {
-                _outputProvider("Here you go! Cold beer.");
-                return;
-            }
-            
-            _outputProvider("Sorry but you're not old enough to drink (in the UK)");
-        }
-
-        private void HandleInvalidAge()
-        {
-            _outputProvider("Could not parse the age provided");
+            _recipeBook.MakeDrink(drink);
         }
     }
 }
