0a6c183971434835375d03d35a4b31d5587bb667
KutukovAV <ka_2005@mail.ru>
2021-02-26 11:04:10 +0300

    Исходный код приложения перед рефакторингом.

---
+++ TheElseKeyword.Example/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TheElseKeyword.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bartender = new Bartender(Console.ReadLine, Console.WriteLine);
+
+            while (true)
+            {
+                bartender.AskForDrink();
+            }
+        }
+    }
+}
